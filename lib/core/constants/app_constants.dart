83d29ae2e26ee4a0c206c36230411a91a5b7c4dd
Zoir <wowdobryy@gmail.com>
2026-05-19 15:49:00 +0900

    fix: change domain name

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  static const baseUrl = 'https://maxtv-api.terexov-tech.ru/api';
+  static const baseUrl = 'https://api.maxtv.co.il/api';
   static const channelsPerPage = 15;
 }
