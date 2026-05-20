798f5e7836f39e34f3a58ddc5847b544e2335810
Zoir <wowdobryy@gmail.com>
2026-05-20 15:41:20 +0900

    fix: update domain

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   static const baseUrl = 'https://api.maxtv.co.il/api';
+  // static const baseUrl = 'https://maxtv-api.terexov-tech.ru/api';
   static const channelsPerPage = 15;
 }
